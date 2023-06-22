61fbaf71a8541ea96806aada16212e0eace5821e
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-06-22 11:32:49 +0300

    add file

---
+++ src/utils.ts
@@ -0,0 +1,36 @@
+import { Types } from 'mongoose';
+import { ResultCode, ResultDto } from './dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export function validID(id: string): boolean {
+  return Types.ObjectId.isValid(id);
+}
+
+export function getResultDto<T>(
+  code: ResultCode,
+  data: T | null = null,
+  errorMessage = '',
+): ResultDto<T> {
+  return {
+    data: data,
+    code: code,
+    errorMessage: errorMessage,
+  };
+}
+
+export function calcResultDto<T>(
+  code: ResultCode,
+  data: T,
+  message: string,
+): T {
+  switch (code) {
+    case ResultCode.Success:
+      return data;
+    case ResultCode.NotFound:
+      throw new HttpException(message, HttpStatus.NOT_FOUND);
+    case ResultCode.ServerError:
+      throw new HttpException(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    default:
+      throw new HttpException('', HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
