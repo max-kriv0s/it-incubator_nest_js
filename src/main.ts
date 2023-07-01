f21eb2cc8574fa191f3622a99a0148fd43378d81
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-01 17:31:38 +0300

    fix AppConfig

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { appSettings } from './app.settings';
 import { getSwaggerJsonFile, swaggerSettings } from './swagger';
+import { AppConfig } from './configuration/app.configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +10,8 @@ async function bootstrap() {
 
   swaggerSettings(app);
 
-  const confiService = app.get(ConfigService);
-  const port = confiService.get('port');
+  const appConfig = app.get(AppConfig);
+  const port = appConfig.getPort();
   await app.listen(port, () => console.log(`Server started on port ${port}`));
 
   getSwaggerJsonFile(app);
