be815cdc200552b946f22608b1a068991e9f4ae1
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-01 17:34:11 +0300

    fix

--- src/feature/auth/decorators/current-user-id.param.decorator.ts
+++ src/feature/auth/decorators/current-user-id.param.decorator.ts
@@ -2,10 +2,11 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 // посмотреть как передать в декаратор параметр
 export const CurrentUserId = createParamDecorator(
-  (data: unknown, context: ExecutionContext): string => {
+  (checkUserId = true, context: ExecutionContext): string => {
     const request = context.switchToHttp().getRequest();
 
-    if (!request.user?.userId) throw new Error('JwtGuard must be used');
+    if (checkUserId && !request.user?.userId)
+      throw new Error('JwtGuard must be used');
     return request.user?.userId as string;
   },
 );
