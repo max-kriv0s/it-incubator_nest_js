ad1372724abb16186a849a9da6c7577ede27f032
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-01 17:31:48 +0300

    added GetFieldError

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,10 @@
 import { Types } from 'mongoose';
+import { FieldError } from './dto';
 
 export function CastToObjectId(value: string): Types.ObjectId {
   return new Types.ObjectId(value);
+}
+
+export function GetFieldError(message: string, field: string): FieldError {
+  return { message, field };
 }
