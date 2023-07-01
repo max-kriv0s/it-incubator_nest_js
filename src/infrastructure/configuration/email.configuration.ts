09185f0fb6c7cde002f443453a63dfb3ece4cdea
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-01 17:39:20 +0300

    added new file email.configuration.ts

---
+++ src/infrastructure/configuration/email.configuration.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { BaseConfig } from 'src/configuration/base.configuration';
+
+@Injectable()
+export class EmailConfig extends BaseConfig {
+  constructor(protected configService: ConfigService) {
+    super(configService);
+  }
+
+  getEmailSettings() {
+    return {
+      TECH_EMAIL: this.configService.get<string>('TECH_EMAIL') ?? '',
+      TECH_EMAIL_PASSWORD:
+        this.configService.get<string>('TECH_EMAIL_PASSWORD') ?? '',
+    };
+  }
+}
