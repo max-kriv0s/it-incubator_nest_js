f0978f23dd660f8173c453eea3e8af9e0788c4c1
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-02 00:36:07 +0300

    added file api-calls.repository.ts

---
+++ src/feature/api-calls/api-calls.repository.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  ApiCalls,
+  ApiCallsDocument,
+  ApiCallsModelType,
+} from './api-calls.schema';
+
+@Injectable()
+export class ApiCallsRepository {
+  constructor(
+    @InjectModel(ApiCalls.name) private ApyCallsModel: ApiCallsModelType,
+  ) {}
+
+  createApiCall(ip: string, url: string): ApiCallsDocument {
+    return this.ApyCallsModel.createApiCall(ip, url, this.ApyCallsModel);
+  }
+
+  async save(apiCall: ApiCallsDocument): Promise<ApiCallsDocument> {
+    return apiCall.save();
+  }
+
+  async countDocuments(
+    ip: string,
+    url: string,
+    verificationDate: Date,
+  ): Promise<number> {
+    return this.ApyCallsModel.countDocuments({
+      IP: ip,
+      URL: url,
+      date: { $gte: verificationDate },
+    });
+  }
+
+  async deleteCalls() {
+    await this.ApyCallsModel.deleteMany({});
+  }
+}
