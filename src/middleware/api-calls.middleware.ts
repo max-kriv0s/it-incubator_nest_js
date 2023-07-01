cbd6d6fef2413ac6749a5812735292e6e51de85f
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-02 00:40:22 +0300

    added file api-calls.middleware.ts

---
+++ src/middleware/api-calls.middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ThrottlerException } from '@nestjs/throttler';
+import { Request, Response, NextFunction } from 'express';
+import { ApiCallsService } from 'src/feature/api-calls/api-calls.service';
+
+@Injectable()
+export class ApiCallsMiddleware implements NestMiddleware {
+  constructor(private readonly apiCallsService: ApiCallsService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const result = await this.apiCallsService.addСallRecord(
+      req.ip,
+      req.originalUrl,
+    );
+    if (!result) throw new ThrottlerException();
+
+    const requestAllowed = await this.apiCallsService.requestAllowed(
+      req.ip,
+      req.originalUrl,
+    );
+    if (!requestAllowed) throw new ThrottlerException();
+    next();
+  }
+}
