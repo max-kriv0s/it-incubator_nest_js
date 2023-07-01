726ea1d594ea57634ff27aee70d10d7c32a171d9
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-02 00:36:50 +0300

    added file api-calls.service.ts

---
+++ src/feature/api-calls/api-calls.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ApiCallsRepository } from './api-calls.repository';
+import { ApiCallsConfig } from './configuration/api-calls.configuration';
+
+@Injectable()
+export class ApiCallsService {
+  constructor(
+    private readonly apiCallsRepository: ApiCallsRepository,
+    private readonly apiCallsConfig: ApiCallsConfig,
+  ) {}
+
+  async addСallRecord(ip: string, url: string): Promise<boolean> {
+    const newApiCall = this.apiCallsRepository.createApiCall(ip, url);
+    await this.apiCallsRepository.save(newApiCall);
+
+    return true;
+  }
+
+  async requestAllowed(ip: string, url: string): Promise<boolean> {
+    const settings = this.apiCallsConfig.getApiCallsSettings();
+
+    const verificationDate = new Date();
+    verificationDate.setSeconds(
+      verificationDate.getSeconds() - settings.QUERY_CHECKING_TIME,
+    );
+
+    const countRequest = await this.apiCallsRepository.countDocuments(
+      ip,
+      url,
+      verificationDate,
+    );
+    return countRequest <= settings.MAX_COUNT_FREQUENT_REQUESTS_FOR_API;
+  }
+}
