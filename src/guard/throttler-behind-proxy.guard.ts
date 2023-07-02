f444931c4edd8ecd78c660cae121c2353c6d2b31
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-02 17:25:47 +0300

    fix ThrottlerBehindProxyGuard

--- src/guard/throttler-behind-proxy.guard.ts
+++ src/guard/throttler-behind-proxy.guard.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): string {
     return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction to meet your own needs
+  }
+  protected async handleRequest(
+    context: ExecutionContext,
+    limit: number,
+    ttl: number,
+  ): Promise<boolean> {
+    return true;
   }
 }
