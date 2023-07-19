37a25cc3782958e2f7863c2561fee4d704538054
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-19 23:56:04 +0300

    added comments and blogger/users

--- src/feature/bloggers/bloggers.controller.ts
+++ src/feature/bloggers/bloggers.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -16,7 +17,10 @@ import { AccessJwtAuthGuard } from '../auth/guard/jwt.guard';
 import { UpdateBlogDto } from '../blogs/dto/update-blog.dto';
 import { CommandBus } from '@nestjs/cqrs';
 import { UpdateExistingBlogByIdCommand } from '../blogs/use-case/update-existing-blog-by-id.usecase';
-import { replyByNotification } from '../../modules/notification';
+import {
+  ResultNotification,
+  replyByNotification,
+} from '../../modules/notification';
 import { CurrentUserId } from '../auth/decorators/current-user-id.param.decorator';
 import { IdValidationPipe } from '../../modules/pipes/id-validation.pipe';
 import { DeleteBlogByIdCommand } from '../blogs/use-case/delete-blog-by-id.usecase';
@@ -35,6 +39,11 @@ import { CreatePostByBlogIdCommand } from '../blogs/use-case/create-post-by-blog
 import { BlogPostUpdateDto } from './dto/blog-post-update.dto';
 import { UpdatePostByIdCommand } from './use-case/update-post-by-id.usecase';
 import { DeletePostByIdCommand } from './use-case/delete-post-by-id.usecase';
+import { PaginatorViewBloggerCommentsDto } from './dto/view-blogger-comments.dto';
+import { BloggerBanUserInputDto } from './dto/blogger-ban-user-input.dto';
+import { BloggerBanUnbanUserCommand } from './use-case/blogger-ban-unban-user.usecase';
+import { GetFieldError } from '../../utils';
+import { BloggerBannedUsersQueryParams } from './dto/blogger-banned-users-query-param.dto';
 
 @UseGuards(AccessJwtAuthGuard)
 @Controller('blogger/blogs')
@@ -153,5 +162,57 @@ export class BloggersController {
       new DeletePostByIdCommand(blogId, postId, userId),
     );
     return replyByNotification(result);
+  }
+
+  @UseGuards(AccessJwtAuthGuard)
+  @Get('comments')
+  async allCommentsForAllPostsInsideBlogs(
+    @Query() queryParams: BloggerQueryParams,
+    @CurrentUserId() userId: string,
+  ): Promise<PaginatorViewBloggerCommentsDto> {
+    return await this.bloggerQueryRepository.allCommentsForAllPostsInsideBlogs(
+      queryParams,
+      userId,
+    );
+  }
+}
+
+@UseGuards(AccessJwtAuthGuard)
+@Controller('blogger/users')
+export class BloggersUsersController {
+  constructor(
+    private commandBus: CommandBus,
+    private readonly bloggerQueryRepository: BloggerQueryRepository,
+  ) {}
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Put(':id/ban')
+  async BanUnbanUser(
+    @CurrentUserId() userId: string,
+    @Param('id', IdValidationPipe) bannedUserId: string,
+    @Body() banUserInputDto: BloggerBanUserInputDto,
+  ) {
+    const result: ResultNotification<null> = await this.commandBus.execute(
+      new BloggerBanUnbanUserCommand(userId, bannedUserId, banUserInputDto),
+    );
+
+    if (result.hasError()) {
+      const error = result.getError();
+      throw new BadRequestException(GetFieldError(error.message, error.field));
+    }
+    return;
+  }
+
+  @Get('blog/:id')
+  async getAllBannedUsersForBlog(
+    @Param('id', IdValidationPipe) id: string,
+    @Query() queryParam: BloggerBannedUsersQueryParams,
+    @CurrentUserId() userId: string,
+  ) {
+    return await this.bloggerQueryRepository.getAllBannedUsersForBlog(
+      id,
+      userId,
+      queryParam,
+    );
   }
 }
