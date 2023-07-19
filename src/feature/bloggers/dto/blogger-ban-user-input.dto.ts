e43030ce5efd5b870f4e413d3e125b91f8e17bb6
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-07-19 23:52:11 +0300

    added the file blogger-ban-user-input.dto.ts

---
+++ src/feature/bloggers/dto/blogger-ban-user-input.dto.ts
@@ -0,0 +1,28 @@
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsMongoId,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { BlogExists } from '../../../feature/posts/validators/blog-exists.validator';
+
+export class BloggerBanUserInputDto {
+  @IsNotEmpty()
+  @IsBoolean()
+  isBanned: boolean;
+
+  @Transform(({ value }) => value?.trim())
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(20)
+  banReason: string;
+
+  @Transform(({ value }) => value?.trim())
+  @IsNotEmpty()
+  @IsString()
+  @IsMongoId()
+  @BlogExists()
+  blogId: string;
+}
