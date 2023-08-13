b06c78bc4b159a87be10e4f2b488dd3b9e575945
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-08-13 23:00:27 +0300

    added Blog entity

---
+++ src/feature/blogs/entities/blog.entity.ts
@@ -0,0 +1,49 @@
+import { BloggerBannedUser } from '../../../feature/bloggers/entities/blogger-banned-user.entity';
+import { Post } from '../../../feature/posts/entities/post.entity';
+import { User } from '../../../feature/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'Blogs' })
+export class Blog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 15 })
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column({ length: 100 })
+  websiteUrl: string;
+
+  @Column({ default: new Date() })
+  createdAt: Date;
+
+  @Column({ default: false })
+  isMembership: boolean;
+
+  @Column()
+  ownerId: number;
+
+  @ManyToOne(() => User, (user) => user.blogs)
+  owner: User;
+
+  @Column({ default: false })
+  isBanned: boolean;
+
+  @Column({ nullable: true })
+  banDate: Date;
+
+  @OneToMany(() => Post, (post) => post.blog)
+  posts: Post[];
+
+  @OneToMany(() => BloggerBannedUser, (bannedUser) => bannedUser.blog)
+  bloggerBannedUsers: BloggerBannedUser[];
+}
