b7fb94d87aa08544c32a5ec7986d9c4d55f54a7c
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-08-13 23:00:43 +0300

    added CommentLike entity

---
+++ src/feature/comments/entities/comment-likes.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Comment } from './comment.entity';
+import { User } from '../../../feature/users/entities/user.entity';
+import { LikeStatus } from '../../../feature/likes/dto/like-status';
+
+@Entity({ name: 'CommentLikes' })
+export class CommentLike {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  commentId: number;
+
+  @ManyToOne(() => Comment, (comment) => comment.likes)
+  comment: Comment;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => User, (user) => user.commentLikes)
+  user: User;
+
+  @Column({ type: 'enum', enum: LikeStatus, default: LikeStatus.None })
+  status: LikeStatus;
+
+  @Column({ default: false })
+  isBanned: boolean;
+}
