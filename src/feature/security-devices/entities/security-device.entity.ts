94aaf8db5c19069fff1b0c1f9d0e352f0f55ff45
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-08-13 23:01:53 +0300

    added SecurityDevice entity

---
+++ src/feature/security-devices/entities/security-device.entity.ts
@@ -0,0 +1,26 @@
+import { User } from '../../../feature/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'SecurityDevices' })
+export class SecurityDevice {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  ip: string;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  lastActiveDate: Date;
+
+  @Column({ nullable: true })
+  expirationTime: Date;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => User, (user) => user.securityDevices)
+  user: User;
+}
