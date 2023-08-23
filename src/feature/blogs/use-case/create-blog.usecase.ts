564f5bda599dddca71876dba354e34809f1ab4de
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-08-23 15:23:23 +0300

    Adapted for Typeorm CreateBlogUseCase

--- src/feature/blogs/use-case/create-blog.usecase.ts
+++ src/feature/blogs/use-case/create-blog.usecase.ts
@@ -7,45 +7,42 @@ import {
   ResultNotification,
 } from '../../../modules/notification';
 import { CreateBlogSqlType } from '../model/blog-sql.model';
-import { BlogsSqlRepository } from '../db/blogs.sql-repository';
+import { BlogsRepository } from '../db/blogs.repository';
+import { UsersRepository } from '../../../feature/users/db/users.repository';
+import { Blog } from '../entities/blog.entity';
 
 export class CreateBlogCommand {
-  constructor(public createDto: CreateBlogDto, public userId: string) {}
+  constructor(public createDto: CreateBlogDto, public userId: number) {}
 }
 
 @CommandHandler(CreateBlogCommand)
 export class CreateBlogUseCase implements ICommandHandler<CreateBlogCommand> {
   constructor(
-    private readonly usersService: UsersService,
-    private readonly blogsSqlRepository: BlogsSqlRepository,
+    private readonly usersRepository: UsersRepository,
+    private readonly blogsRepository: BlogsRepository,
   ) {}
 
   async execute(
     command: CreateBlogCommand,
-  ): Promise<ResultNotification<string>> {
+  ): Promise<ResultNotification<number>> {
     await validateOrRejectModel(command.createDto, CreateBlogDto);
 
-    const creationResult = new ResultNotification<string>();
+    const creationResult = new ResultNotification<number>();
 
-    const user = await this.usersService.findUserById(+command.userId);
+    const user = await this.usersRepository.findUserById(+command.userId);
     if (!user) {
       creationResult.addError('User not found', ResultCodeError.NotFound);
       return creationResult;
     }
 
-    const data: CreateBlogSqlType = {
-      name: command.createDto.name,
-      description: command.createDto.description,
-      websiteUrl: command.createDto.websiteUrl,
-      ownerId: user.id.toString(),
-    };
+    const newBlog = new Blog();
+    newBlog.name = command.createDto.name;
+    newBlog.description = command.createDto.description;
+    newBlog.websiteUrl = command.createDto.websiteUrl;
+    newBlog.ownerId = user.id;
+    await this.blogsRepository.createBlog(newBlog);
 
-    const newBlog = await this.blogsSqlRepository.createBlog(data);
-    if (!newBlog) {
-      creationResult.addError('The blog is not created');
-    } else {
-      creationResult.addData(newBlog.id);
-    }
+    creationResult.addData(newBlog.id);
     return creationResult;
   }
 }
